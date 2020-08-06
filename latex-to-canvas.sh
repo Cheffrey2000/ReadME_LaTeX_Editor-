dcdd6faeb27311ddd0feb1dd29c3b9c02d263556
cheffrey2000 <cheffreyh@gmail.com>
2020-08-06 12:24:01 -0400

    updated shell script

--- latex-to-canvas.sh
+++ latex-to-canvas.sh
@@ -1,43 +1,37 @@
 """
 This script is designed to take in a local directory of a cloned lesson repo and the lesson number
 It will then:
-    -iterate through each repo
     -checkout the canvas branch
     -fix the LaTeX rendering in the README.md
-    -and create a lesson on canvas with the provided course number
-Example: bash latex-to-canvas.sh phase2-repos 343  - this will use the post-grad folder and pass 154 as the course id
-Requires github-to-canvas gem
+    -and update the lesson on canvas with the provided course number
+Example: bash latex-to-canvas.sh ../dsc-bayes-theorem 343  - this will change directory into /dsc-bayes-theorem and pass 343 as the course id
+Requires github-to-canvas gem and latex-to-canvas.py script
 """
 
 
-if [ $# -eq 0 ] || [ $# -eq 1 ] || [ $# -eq 2 ]
+if [ $# -eq 0 ] || [ $# -eq 1 ]
   then
-    echo "Three arguments required - the folder name to iterate through, the canvas course id, and the LaTeX inticator character"
-    echo "Example: bash latex-to-canvas.sh phase2-repos 343 "
+    echo "Three arguments required - the folder name and the canvas course id"
+    echo "Example: bash latex-to-canvas.sh ../dsc-bayes-theorem 343"
     exit 1
 fi
 
 
-REPOS=$PWD/$1/*
 
-cd $1
-
-for r in $REPOS
-do
   
-  cd $r
-  git checkout -b canvas
+cd $r
+git checkout -b canvas
 
-  python ~/latex-to-canvas.py README.md $3
+python ~/projects/latex-to-canvas.py README.md
 
-  git add .
-  git commit -m 'Corrected LaTeX rendering with latex-to-canvas script'
+git add .
+git commit -m 'Corrected LaTeX rendering on canvas branch with latex-to-canvas script'
 
-   
-  github-to-canvas -c $2 -slr --type page -b canvas
   
-  # Avoid limits with sleep for large amounts of repos
-  sleep 5;
+github-to-canvas -a --course $2 -lr -b canvas
+
+# Avoid limits with sleep for large amounts of repos
+sleep 5;
 done
 
 
